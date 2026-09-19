033f3dc9bda9275b54410d1eba5336f0f8ba2f47
Pierre Kostantine <150958805+pKostantine@users.noreply.github.com>
2026-09-19 15:24:05 -0400

    Default new releases to ASAP

--- src/context/WorkspaceContext.tsx
+++ src/context/WorkspaceContext.tsx
@@ -30,6 +30,7 @@ export const emptyDraft = (): CreatorDraft => ({
   hymnId: '',
   localizedTitle: { en: '', ar: '', cop: '', fr: '' },
   media: [],
+  releaseTimingMode: 'asap',
   scheduledReleaseAt: defaultReleaseDate(),
   originalReleaseDate: '',
 });
